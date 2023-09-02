071d2a6119af1b78364fc97e323d5eb0c3916253
Marcio Curvello <marciocurvello@gmail.com>
2023-09-02 13:23:45 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Marcio Curvello on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
